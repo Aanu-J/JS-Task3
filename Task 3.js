08ac1ad04d8cd978276c0212632d8cd71da72c05
Aanu-J <61972927+Aanu-J@users.noreply.github.com>
2020-04-19 19:56:01 +0100

    Add files via upload

---
+++ Task 3.js
@@ -0,0 +1,11 @@
+var
+var input = prompt ("Enter any integer");
+if (Number.isInteger(input) == 0){
+    input = "Error enter an integer"
+} else{
+    var i;
+    for (i=0; i=100; i++) {
+    }
+}
+
+
