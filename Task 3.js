e2c40bbcdd35467e4cdcea7fdccd98a495792999
Aanu-J <61972927+Aanu-J@users.noreply.github.com>
2020-04-19 22:29:58 +0100

    Update Task 3.js

--- Task 3.js
+++ Task 3.js
@@ -1,46 +1,34 @@
-//divResult problem in JavaScript
-
-//start the function
 function divResult(num){
+const result=[];
 
-    //create an array
-      const result = [];
-      
-    //begin the loop
-      for (let i = 1; i <= num; i++) {
+for (let i = 1; i <= num; i++) {
+	let isDivisible = false;
+	const val = [];
 
-        //declear a boolean value
-        let isDivisible = false;
-        const val = [];
-        //detect values divisible by 2
-        if (i % 2 == 0) {
-          isDivisible = true;
-          val.push("yu");
-        }
-        //detect values divisible by 3
-        if (i % 3 == 0) {
-          isDivisible = true;
-          val.push("gi");
-        }
-        //detect values divisible by 5
-        if (i % 5 == 0) {
-          isDivisible = true;
-          val.push("oh");
-        }
-        //
-        if (isDivisible) {
-          result.push(val.join('-'))
-        //detect values not divisible by 2, 3 or 5
-        } else {
-          result.push(i)
-        }
-      }
-      //print array
-      console.log(result);
+    if (i % 2 == 0) {
+		isDivisible = true;
+        val.push("yu");
     }
-    
-    //call 100 as an argument
-    divResult (100);
-    
-    //call 100892 as an argument
-    divResult (100892);
+	
+	if (i % 3 == 0){
+		isDivisible = true;
+        val.push("gi");
+    }
+	
+	if (i % 5 == 0){
+		isDivisible = true;
+        val.push("oh");
+    }
+
+	if (isDivisible) {
+		result.push(val.join('-'))
+	} else {
+		result.push(i)
+	}
+}
+
+console.log(result);
+return result;
+}
+divResult(100);
+divResult(560);
